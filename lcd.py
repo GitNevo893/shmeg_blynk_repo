a25aa30dcedfad6b610dc0434a737bbcecaaaa97
GitNevo893 <warrior.sharkies@gmail.com>
2026-04-13 12:48:06 +0300

    Update lcd.py

--- lcd.py
+++ lcd.py
@@ -1,7 +1,17 @@
 import smbus
-bus=smbus.SMBus(1)
-adress=0x27
-bus.write_byte(adress, 0x01)
-from RPLCD.i2c import CharLCD
-lcd = CharLCD('PCF8574', 0x27)
-lcd.write_string("Hello!")
+from time import sleep
+addr = 0x27
+bus = smbus.SMBus(1)
+def cmd(x):
+    bus.write_byte(addr, x | 0x08)
+def send(x, mode=0):
+    high = mode | (x & 0xF0) | 0x08
+    low  = mode | ((x<<4) & 0xF0) | 0x08
+    bus.write_byte(addr, high); bus.write_byte(addr, high | 0x04); bus.write_byte(addr, high)
+    bus.write_byte(addr, low);  bus.write_byte(addr, low  | 0x04); bus.write_byte(addr, low)
+# init
+for c in [0x33,0x32,0x28,0x0C,0x01]:
+    send(c); sleep(0.005)
+# print text
+for ch in "Hello!":
+    send(ord(ch), 1)
