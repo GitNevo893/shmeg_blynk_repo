3606691c080d0dbd9e32549cf641d487ddfd142a
GitNevo893 <warrior.sharkies@gmail.com>
2026-04-13 13:19:39 +0300

    Update lcd.py

--- lcd.py
+++ lcd.py
@@ -13,5 +13,5 @@ def send(x, mode=0):
 for c in [0x33,0x32,0x28,0x0C,0x01]:
     send(c); sleep(0.005)
 # print text
-for ch in "Hello!":
+for ch in "if you read this you are gay":
     send(ord(ch), 1)
