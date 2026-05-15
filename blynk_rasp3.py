5bc42520308e9a37b43b315e798da9fb85eaae65
GitNevo893 <warrior.sharkies@gmail.com>
2026-05-15 08:21:59 +0300

    Update blynk_rasp3.py

--- blynk_rasp3.py
+++ blynk_rasp3.py
@@ -199,6 +199,7 @@ def read_updates():
             old_date=old_date.split(",")
             for i in range(3):
                 if int(new_date[i])>int(old_date[i]):
+                    print("yo?")
                     return 
             date_str=make_date(new_date)
             blynk_write(cell_date[cell_num], date_str)
